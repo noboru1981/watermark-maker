11e82a9231ffde0cbaf88fc049e05fb5aa7524cd
Noboru Yamaguchi <noboru@skip-f.net>
2019-03-08 02:28:36 +0900

    Safariでファイル名が表示されなかったのを修正

--- src/components/FileSelector.jsx
+++ src/components/FileSelector.jsx
@@ -12,17 +12,16 @@ class FileSelector extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            fileName: ""
+        }
+
+        this.file = React.createRef();
+        this.fileName = React.createRef();
+        this.button = React.createRef();
+
         this.fileSelectHandler = this.fileSelectHandler.bind(this);
         this.fileChangeHandler = this.fileChangeHandler.bind(this);
-    }
-
-    /**
-     * component読み込み時
-     */
-    componentDidMount() {
-        this.fileSelector = document.getElementById('fileSelector');
-        this.filenameText = document.getElementById('filename');
-        this.selectButton = document.getElementById('selectButton');
     }
 
     /**
@@ -30,50 +29,60 @@ class FileSelector extends Component {
      * @param event
      */
     fileChangeHandler(event) {
-        const files = this.fileSelector.files;
+        const file = event.target.files[0];
 
-        let fileNames = '';
+        this.setState(() => {
+            const newState = {};
+            if (file) {
+                // ファイルが選択された場合はファイル事態とファイル名をセット
+                newState.file = file;
+                newState.fileName = file.name;
+            } else {
+                // ファイル未選択の場合はファイル名を空にしてstate更新
+                newState.fileName = "";
+            }
 
-        // filesは単一なので直値していでも構わないが一応for...ofループでファイル名を取る
-        for (let file of files) {
-            fileNames += file.name
-        }
-
-        // 表示用ファイル名欄への出力
-        this.filenameText.value = fileNames;
+            return newState;
+        });
     }
 
     /**
-     * ファイル選択ボタンクリック・テキストクリックが行われた際のイベントハンドラ
+     * ファイル選択ボタンクリック・テキストクリックが行われた際
      * @param event
      */
     fileSelectHandler(event) {
         // 表示されている要素のフォーカスを外し、ファイル選択ダイアログを表示
-        this.filenameText.blur();
-        this.selectButton.blur();
-        this.fileSelector.click();
+        this.file.current.click();
+        this.fileName.current.blur();
+        this.button.current.blur();
     }
 
+    /**
+     * render
+     * @returns {*}
+     */
     render() {
         return (
             <div>
                 <Form.Control type="file"
                               id="fileSelector"
                               onChange={this.fileChangeHandler}
+                              ref={this.file}
                               style={{display: "none"}}
                 />
-                <label htmlFor="fileSelector">
-                    <InputGroup className="mb-3">
-                        <InputGroup.Prepend onClick={this.fileSelectHandler}>
-                            <Button id={'selectButton'}>Select Image</Button>
-                        </InputGroup.Prepend>
-                        <FormControl aria-describedby="basic-addon1"
-                                     id={'filename'}
-                                     readOnly={true}
-                                     onClick={this.fileSelectHandler}
-                        />
-                    </InputGroup>
-                </label>
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend onClick={this.fileSelectHandler}>
+                        <Button id={'selectButton'}
+                                ref={this.button}>Select Image</Button>
+                    </InputGroup.Prepend>
+                    <FormControl aria-describedby="basic-addon1"
+                                 id={'filename'}
+                                 ref={this.fileName}
+                                 readOnly={true}
+                                 onClick={this.fileSelectHandler}
+                                 value={this.state.fileName}
+                    />
+                </InputGroup>
             </div>
         );
     }
