a50482c03a4b2436f1cab4761fe8d63c6f954c6d
Noboru Yamaguchi <yamaguchi@logical-studio.com>
2019-03-15 21:40:00 +0900

    位置選択コンポーネント作成

--- src/components/PositionSelector.jsx
+++ src/components/PositionSelector.jsx
@@ -1,28 +1,71 @@
 import React, {Component} from "react";
+import {Row, Form, Col, ToggleButton, ButtonGroup, FormGroup} from "react-bootstrap";
+
+const ButtonGroupParentStyle = {
+    display: "flex"
+};
+
+const ButtonGroupStyle = {
+    width: "100%"
+};
+
+const ButtonStyle = {
+    width: "100%",
+    display: "block"
+};
 
 /**
- * 
+ *
  */
 class PositionSelector extends Component {
     render() {
         return (
-            <div>
-                <div>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                </div>
-                <div>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                </div>
-                    <div>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                </div>
-            </div>
+            <Row>
+                <Col sm={4}>
+                    <FormGroup>
+                        <Form.Label>表示位置</Form.Label>
+                        <Row>
+                            <Col style={ButtonGroupParentStyle}>
+                                <ButtonGroup style={ButtonGroupStyle} toggle>
+                                    <ToggleButton type={"radio"} name={"captionPosition"} value={7} style={ButtonStyle}>↖</ToggleButton>
+                                </ButtonGroup>
+                                <ButtonGroup style={ButtonGroupStyle} toggle>
+                                    <ToggleButton type={"radio"} name={"captionPosition"} value={8} style={ButtonStyle}>↑</ToggleButton>
+                                </ButtonGroup>
+                                <ButtonGroup style={ButtonGroupStyle} toggle>
+                                    <ToggleButton type={"radio"} name={"captionPosition"} value={9} style={ButtonStyle}>↗</ToggleButton>
+                                </ButtonGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col style={ButtonGroupParentStyle}>
+                                <ButtonGroup style={ButtonGroupStyle} toggle>
+                                    <ToggleButton type={"radio"} name={"captionPosition"} value={4} style={ButtonStyle}>←</ToggleButton>
+                                </ButtonGroup>
+                                <ButtonGroup style={ButtonGroupStyle} toggle>
+                                    <ToggleButton type={"radio"} name={"captionPosition"} value={5} style={ButtonStyle}>・</ToggleButton>
+                                </ButtonGroup>
+                                <ButtonGroup style={ButtonGroupStyle} toggle>
+                                    <ToggleButton type={"radio"} name={"captionPosition"} value={6} style={ButtonStyle}>→</ToggleButton>
+                                </ButtonGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col style={ButtonGroupParentStyle}>
+                                <ButtonGroup style={ButtonGroupStyle} toggle>
+                                    <ToggleButton type={"radio"} name={"captionPosition"} value={1}>↙</ToggleButton>
+                                </ButtonGroup>
+                                <ButtonGroup style={ButtonGroupStyle} toggle>
+                                    <ToggleButton type={"radio"} name={"captionPosition"} value={2}>↓</ToggleButton>
+                                </ButtonGroup>
+                                <ButtonGroup style={ButtonGroupStyle} toggle>
+                                    <ToggleButton type={"radio"} name={"captionPosition"} value={3}>↘</ToggleButton>
+                                </ButtonGroup>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                </Col>
+            </Row>
         );
     }
 }
