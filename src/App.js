dc1f62febfc515da7411485eceae8521ab5145fa
Noboru Yamaguchi <yamaguchi@logical-studio.com>
2019-03-15 21:39:28 +0900

    フォント選択作成

--- src/App.js
+++ src/App.js
@@ -1,71 +1,104 @@
-import React, {Component} from 'react'
-import {Container, Row, Col} from 'react-bootstrap'
-import DrawArea from "./components/DrawArea"
-import FileSelector from "./components/FileSelector"
+import React, {Component} from "react";
+import {Container, Row, Col} from "react-bootstrap";
+import DrawArea from "./components/DrawArea";
+import FileSelector from "./components/FileSelector";
+import TextInput from "./components/TextInput";
+import PositionSelector from "./components/PositionSelector";
+import FontSelector from "./components/FontSelector";
 
+/**
+ *
+ */
 class App extends Component {
+    /**
+     *
+     * @param props
+     */
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             file: false,
             imgSize: {
                 height: 0,
                 width: 0
-            }
-        }
+            },
+            fontFace: "serif"
+        };
 
-        this.fileChangeHandler = this.fileChangeHandler.bind(this)
+        this.fileChangeHandler = this.fileChangeHandler.bind(this);
+        this.fontChangeHandler = this.fontChangeHandler.bind(this);
     }
 
+    /**
+     *
+     * @param event
+     */
     fileChangeHandler(event) {
-        const file = event.target.files[0]
+        const file = event.target.files[0];
 
         // chromeはダイアログを閉じるとファイル選択が解除されるので空値セット
         if (!file) {
             this.setState((currentState) => {
-                const newState = currentState
-                newState.file = new Blob()
+                const newState = currentState;
+                newState.file = new Blob();
                 newState.imgSize = {
                     height: 0,
                     width: 0
-                }
+                };
 
-                return newState
-            })
-            return
+                return newState;
+            });
+            return;
         }
 
-        const allowedFileTypes = ["image/png", "image/jpeg", "image/gif"]
-        const isImageSelected = allowedFileTypes.indexOf(file.type) > -1
-        const img = new Image()
+        const allowedFileTypes = ["image/png", "image/jpeg", "image/gif"];
+        const isImageSelected = allowedFileTypes.indexOf(file.type) > -1;
+        const img = new Image();
 
         if (isImageSelected) {
             img.onload = () => {
-                this.setState((currentState) => {
-                    let newState = currentState
+                this.setState(currentState => {
+                    let newState = currentState;
                     newState.imgSize = {
                         height: img.naturalHeight,
                         width: img.naturalWidth
-                    }
-                    return newState
-                })
+                    };
+                    return newState;
+                });
 
-            }
-            img.src = URL.createObjectURL(file)
+            };
+            img.src = URL.createObjectURL(file);
         }
 
         this.setState((currentState) => {
-            const newState = currentState
+            const newState = currentState;
             if (isImageSelected) {
-                newState.file = file
+                newState.file = file;
             } else {
-                newState.file = false
+                newState.file = false;
             }
 
-            return newState
-        })
+            return newState;
+        });
     }
 
+    /**
+     * フォント変更
+     * @param event
+     */
+    fontChangeHandler(event) {
+        const newValue = event.target.value;
+        this.setState(currentState => {
+            const newState = currentState;
+            newState.fontFace = newValue;
+            return newState;
+        });
+    }
+
+    /**
+     * render
+     * @returns {*}
+     */
     render() {
         return (
             <Container className="App">
@@ -76,12 +109,27 @@ class App extends Component {
                                   height={this.state.imgSize.height}/>
                     </Col>
                     <Col>
-                        <FileSelector onChange={this.fileChangeHandler}/>
+                        <Row>
+                            <Col>
+                                <FileSelector onChange={this.fileChangeHandler}/>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <TextInput/>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <PositionSelector/>
+                            </Col>
+                        </Row>
+                        <FontSelector onChange={this.fontChangeHandler}/>
                     </Col>
                 </Row>
             </Container>
-        )
+        );
     }
 }
 
-export default App
+export default App;
--- src/components/FontSelector.jsx
+++ src/components/FontSelector.jsx
@@ -1,12 +1,23 @@
 import React, {Component} from "react";
 
+const fonts = [
+    {value: "serif", label: "serif"},
+    {value: "sans-serif", label: "sans-serif"}
+];
+
 /**
- * 
+ *
  */
 class FontSelector extends Component {
+    /**
+     *
+     * @returns {*}
+     */
     render() {
         return (
-            <select></select>
+            <select onChange={this.props.onChange}>
+                {fonts.map((r, k) => <option value={r.value} key={k}>{r.label}</option>)}
+            </select>
         );
     }
 }
